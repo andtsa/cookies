0abc94ea68fe6fd253f24dfa3657053bcba05a76
Andreas Tsatsanis <code@andreats.com>
2026-06-04 13:18:15 +0200

    crawler stats

--- main.sh
+++ main.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 ./.venv/bin/python3 scripts/get_cookies.py \
-    -c 30 --batch-size 30 --force-concurrency \
+    --batch-size 30 \
     --failed-sites="cookies_data/failed_sites.csv" \
     --category="popular" --country="Netherlands" -i ./list_websites_1M.csv
+    --timeout-ms=5000
+
+    # -c 12 --force-concurrency \
